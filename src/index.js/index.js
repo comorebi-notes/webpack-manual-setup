fefb9857bfff07bb9ddce5748fe6fbd8f5116fa3
kero <kero@comore.bi>
2020-09-17 11:41:09 +0900

    init

---
+++ src/index.js/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Top = () => <div>test</div>
+
+const domContainer = document.getElementById('react')
+ReactDOM.render(<Top />, domContainer)
