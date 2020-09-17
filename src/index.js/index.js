ef07ddf2e69bf374f1de04b12c6e5deb0bc541cc
kero <kero@comore.bi>
2020-09-17 12:07:44 +0900

    webpack-dev-server

--- src/index.js/index.js
+++ src/index.js/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Top = () => <div>test</div>
+const Top = () => (
+  <div style={{ backgroundColor: 'black', color: 'yellow' }}>
+    <h1>test</h1>
+  </div>
+)
 
 const domContainer = document.getElementById('react')
 ReactDOM.render(<Top />, domContainer)
